6fcb0d7d301f0574ad72db3ccf6934ff0b8635a6
Akshay Gurav <mathsjourneywithakshay@gmail.com>
2025-07-11 19:36:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -68,4 +68,4 @@ def convert_video():
             return jsonify({'error': 'Internal server error'}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True)
